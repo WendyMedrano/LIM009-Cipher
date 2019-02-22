27dbec0ce0d44ae303a9e8028444d5b4e5212472
wendy <wendy.medrano@unmsm.edu.pe>
2019-02-22 16:49:31 -0500

    encode MAY y min

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,13 @@
 window.cipher = {
   encode: (offset, string) => {
-    /* Acá va tu código */
+    /* Codifica para AZ */
+    for(let i = 0; i < string.length; i++) {
+      if(65<=string.charCodeAt(i) <=90){
+      let numdecif = (string.charCodeAt(i) - 65 + offset) % 26 + 65; // 70
+      let palabraCifrada = String.fromCharCode(numdecif);
+          document.write(palabraCifrada);
+
+      }
   },
   decode: (offset, string) => {
     /* Acá va tu código */
