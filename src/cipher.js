00d0ea2abbc1a3fd02052e536c9c3ec6502bacab
wendy <wendy.medrano@unmsm.edu.pe>
2019-02-23 22:05:08 -0500

    mayus minus y espacio

--- src/cipher.js
+++ src/cipher.js
@@ -2,13 +2,26 @@ window.cipher = {
   encode: (offset, string) => {
     /* Codifica para AZ */
     for(let i = 0; i < string.length; i++) {
-      if(65<=string.charCodeAt(i) <=90){
-      let numdecif = (string.charCodeAt(i) - 65 + offset) % 26 + 65; // 70
-      let palabraCifrada = String.fromCharCode(numdecif);
-          document.write(palabraCifrada);
+      if(65 <= string.charCodeAt(i) && string.charCodeAt(i)<= 90){
+        let numdecif = (string.charCodeAt(i) - 65 + offset) % 26 + 65; // 70
+        let palabraCifrada = String.fromCharCode(numdecif);
+        document.write(palabraCifrada);
+        
+        }
+     else if(97 <= string.charCodeAt(i) && string.charCodeAt(i)<= 122){
+        let numdecif = (string.charCodeAt(i) - 97 + offset) % 26 + 97; 
+        let palabraCifrada = String.fromCharCode(numdecif);
+        document.write(palabraCifrada);
+       }
+     else if (string.charCodeAt(i) ===32) {
+       document.write(" ")
+       
+       
+     }
+     }
+     }
 
-      }
-  },
+      
   decode: (offset, string) => {
     /* Acá va tu código */
   }
