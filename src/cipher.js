1f41b6fb9691f734e036d7db546cd220d56cfce3
wendy <wendy.medrano@unmsm.edu.pe>
2019-02-25 17:25:41 -0500

    HTML con imagen y btns de encode y decode que no funcionan

--- src/cipher.js
+++ src/cipher.js
@@ -3,22 +3,23 @@
   
   encode: (offset, string) => {
     /* Codifica para AZ az y space */
-    const screenRootprincipal= document.getElementById("rootprincipal").display="none";
-    const screenEncript = document.getElementById("encript").display="block";
-    
+       
     for(let i = 0; i < string.length; i++) {
+      //mayusculas
       if(65 <= string.charCodeAt(i) && string.charCodeAt(i)<= 90){
         let numdecif = (string.charCodeAt(i) - 65 + offset) % 26 + 65; // 70
         let palabraCifrada = String.fromCharCode(numdecif);
         document.write(palabraCifrada);
         }
+        //minusculas
      else if(97 <= string.charCodeAt(i) && string.charCodeAt(i)<= 122){
         let numdecif = (string.charCodeAt(i) - 97 + offset) % 26 + 97; 
         let palabraCifrada = String.fromCharCode(numdecif);
         document.write(palabraCifrada);
        }
+       //espacio
      else if (string.charCodeAt(i) ===32) {
-       document.write(" ")
+       document.write(" ");
      }
      }
      }
@@ -26,5 +27,4 @@
       
   decode: (offset, string) => {
     /* Acá va tu código */
-  }
-};
+  };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 /* Acá va tu código */
+window.onload =()=>{
+document.getElementById("encript").style.display="none";
+}
+document.getElementById("startBtn").addEventListener("click",()=>{
+document.getElementById("rootprincipal").style.display="none";
+document.getElementById("encript").style.display="block";
+
+
+})
