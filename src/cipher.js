a32cf5e6795a94342a564e66766912fcb74fb00b
wendy <wendy.medrano@unmsm.edu.pe>
2019-02-25 00:21:40 -0500

    text + buttom

--- src/cipher.js
+++ src/cipher.js
@@ -1,12 +1,16 @@
-window.cipher = {
+ 
+  window.cipher = {
+  
   encode: (offset, string) => {
-    /* Codifica para AZ */
+    /* Codifica para AZ az y space */
+    const screenRootprincipal= document.getElementById("rootprincipal").display="none";
+    const screenEncript = document.getElementById("encript").display="block";
+    
     for(let i = 0; i < string.length; i++) {
       if(65 <= string.charCodeAt(i) && string.charCodeAt(i)<= 90){
         let numdecif = (string.charCodeAt(i) - 65 + offset) % 26 + 65; // 70
         let palabraCifrada = String.fromCharCode(numdecif);
         document.write(palabraCifrada);
-        
         }
      else if(97 <= string.charCodeAt(i) && string.charCodeAt(i)<= 122){
         let numdecif = (string.charCodeAt(i) - 97 + offset) % 26 + 97; 
@@ -15,12 +19,10 @@ window.cipher = {
        }
      else if (string.charCodeAt(i) ===32) {
        document.write(" ")
-       
-       
      }
      }
      }
-
+     }
       
   decode: (offset, string) => {
     /* Acá va tu código */
