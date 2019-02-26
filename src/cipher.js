9be055e58dc40091c423b45aeb3f8d327d8db361
wendy <wendy.medrano@unmsm.edu.pe>
2019-02-26 03:23:44 -0500

    cifrado q no funciona

--- src/cipher.js
+++ src/cipher.js
@@ -1,19 +1,19 @@
  
   window.cipher = {
   
-  encode: (offset, string) => {
+  encode: (string,offset) => {
     /* Codifica para AZ az y space */
-       
+     const valueOffset = parseInt(offset);  
     for(let i = 0; i < string.length; i++) {
       //mayusculas
       if(65 <= string.charCodeAt(i) && string.charCodeAt(i)<= 90){
-        let numdecif = (string.charCodeAt(i) - 65 + offset) % 26 + 65; // 70
+        let numdecif = (string.charCodeAt(i) - 65 + valueOffset) % 26 + 65; // 70
         let palabraCifrada = String.fromCharCode(numdecif);
         document.write(palabraCifrada);
         }
         //minusculas
      else if(97 <= string.charCodeAt(i) && string.charCodeAt(i)<= 122){
-        let numdecif = (string.charCodeAt(i) - 97 + offset) % 26 + 97; 
+        let numdecif = (string.charCodeAt(i) - 97 + valueOffset) % 26 + 97; 
         let palabraCifrada = String.fromCharCode(numdecif);
         document.write(palabraCifrada);
        }
@@ -23,8 +23,9 @@
      }
      }
      }
+    
      }
-      
+     encode ();
   decode: (offset, string) => {
     /* Acá va tu código */
   };
--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ document.getElementById("encript").style.display="none";
 document.getElementById("startBtn").addEventListener("click",()=>{
 document.getElementById("rootprincipal").style.display="none";
 document.getElementById("encript").style.display="block";
-
-
+})
+//Vammo a encriptar
+const texttoDecode =document.getElementById("inboxarea");
+const numdecif = document.getElementById("offset");
+const textDecifrado=document.getElementById("desboxarea");
+document.getElementById("btnEncode").addEventListener("click",()=>{
+const mensajeCapturado= texttoDecode.value;
+const offsetCapturado= numdecif.value;
+textDecifrado.innerHTML =  encode(texttoDecode.value, numdecif.value);
 })
