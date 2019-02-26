1bc44d18dd0e0ee34837b554d374eed47cd90a0f
wendy <wendy.medrano@unmsm.edu.pe>
2019-02-26 13:05:13 -0500

    Decodificaaa por fiiiin

--- src/cipher.js
+++ src/cipher.js
@@ -3,29 +3,53 @@
   
   encode: (string,offset) => {
     /* Codifica para AZ az y space */
-     const valueOffset = parseInt(offset);  
+    let desboxing="";
+     let valueOffset = parseInt(offset);  
     for(let i = 0; i < string.length; i++) {
       //mayusculas
       if(65 <= string.charCodeAt(i) && string.charCodeAt(i)<= 90){
         let numdecif = (string.charCodeAt(i) - 65 + valueOffset) % 26 + 65; // 70
         let palabraCifrada = String.fromCharCode(numdecif);
-        document.write(palabraCifrada);
+        desboxing+=palabraCifrada;
         }
         //minusculas
      else if(97 <= string.charCodeAt(i) && string.charCodeAt(i)<= 122){
         let numdecif = (string.charCodeAt(i) - 97 + valueOffset) % 26 + 97; 
         let palabraCifrada = String.fromCharCode(numdecif);
-        document.write(palabraCifrada);
+        desboxing+=palabraCifrada;
        }
        //espacio
      else if (string.charCodeAt(i) ===32) {
-       document.write(" ");
+      desboxing+=" ";
      }
      }
-     }
+return desboxing;
+     },
     
-     }
-     encode ();
-  decode: (offset, string) => {
-    /* Acá va tu código */
-  };
+    
+     decode: (string,offset) => {
+      /* Codifica para AZ az y space */
+      let desboxing2="";
+       let valueOffset2 = parseInt(offset);  
+      for(let i = 0; i < string.length; i++) {
+        //mayusculas
+        if(65 <= string.charCodeAt(i) && string.charCodeAt(i)<= 90){
+          let numdecif2 = (string.charCodeAt(i) - 65 - valueOffset2) % 26 + 65; // 70
+          let palabraCifrada2 = String.fromCharCode(numdecif2);
+          desboxing2+=palabraCifrada2;
+          }
+          //minusculas
+       else if(97 <= string.charCodeAt(i) && string.charCodeAt(i)<= 122){
+          let numdecif2 = (string.charCodeAt(i) - 97 - valueOffset2) % 26 + 97; 
+          let palabraCifrada2 = String.fromCharCode(numdecif2);
+          desboxing2+=palabraCifrada2;
+         }
+         //espacio
+       else if (string.charCodeAt(i) ===32) {
+        desboxing2+=" ";
+       }
+       }
+  return desboxing2;
+       }
+      }
+    
--- src/index.js
+++ src/index.js
@@ -7,11 +7,29 @@ document.getElementById("rootprincipal").style.display="none";
 document.getElementById("encript").style.display="block";
 })
 //Vammo a encriptar
-const texttoDecode =document.getElementById("inboxarea");
-const numdecif = document.getElementById("offset");
-const textDecifrado=document.getElementById("desboxarea");
-document.getElementById("btnEncode").addEventListener("click",()=>{
-const mensajeCapturado= texttoDecode.value;
-const offsetCapturado= numdecif.value;
-textDecifrado.innerHTML =  encode(texttoDecode.value, numdecif.value);
-})
+let btnEncode= document.getElementById("btnEncode");
+btnEncode.addEventListener("click",encode);
+function encode(){
+    
+        const mensajeCapturado =document.getElementById("inboxarea").value;
+        const offsetCapturado = document.getElementById("offset").value;
+        const desboxing=cipher.encode(mensajeCapturado,offsetCapturado);
+
+        document.getElementById("desboxarea").style.display="block";
+        document.getElementById("desboxarea").innerHTML=desboxing;   
+
+    }
+
+//Vammo a desencriptar
+let btnDecode= document.getElementById("btnDecode");
+btnDecode.addEventListener("click",decode);
+function decode(){
+    
+        const mensajeCapturado2 =document.getElementById("inboxarea").value;
+        const offsetCapturado2 = document.getElementById("offset").value;
+        const desboxing2=cipher.decode(mensajeCapturado2,offsetCapturado2);
+
+        document.getElementById("desboxarea").style.display="block";
+        document.getElementById("desboxarea").innerHTML=desboxing2;   
+
+    }
